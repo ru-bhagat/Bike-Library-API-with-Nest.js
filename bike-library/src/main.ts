d33d60038c5f986c19ee23f6f48770d0b7ba9e30
Rutuja Bhagat <rutujabhagatco2021@gmail.com>
2024-10-09 10:21:22 +0530

    Create main.ts

---
+++ bike-library/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Bike Library API')
+    .setDescription('The Bike Library API documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
